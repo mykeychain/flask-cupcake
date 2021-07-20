8664b6f6d2f6b47daf1bd54e72be5287ff65d1b9
Wukongthethird <simonzhang9518@gmail.com>
2021-07-19 17:09:04 -0700

    cupcakes

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -6,7 +6,9 @@ const $cupcakeList = $("#cupcake-list")
 
 let cupcakeList;
 
-// returns a list of cupcake flavors
+
+// returns a list of cupcake flavors return array of cupcake flavors
+// [cupcake.flavor, cupcake.flavor, ......]
 async function getCupcakes() {
     let response = await axios.get(BASE_URL);
 
@@ -14,20 +16,49 @@ async function getCupcakes() {
 }
 
 
-// accepts array of cupcakes
-function showCupcakeList(cupcakes) {
+// accepts array of cupcakes 
+// shows sounds like there is a  hide function
+//append cupcakes
+
+function appendCupcakesList(cupcakes) {
 
     for (let cupcake of cupcakes) {
-        let entry = $(`<li>${cupcake}</li>`)
-        $cupcakeList.append(entry)
+        let $cupcake = $(`<li>${cupcake}</li>`);
+        $cupcakeList.append($cupcake);
     }
 }
 
 // handles page load
-async function loadPage(){
-    let cupcakes = await getCupcakes()
-    showCupcakeList(cupcakes)
+//populate all cupcake
+
+async function populateCupcakesList(){
+    let cupcakes = await getCupcakes();
+    appendCupcakesList(cupcakes);
 }
 
+async function addCupcakeForm(evt){
+    evt.preventDefault();
 
-$(document).ready(loadPage)
+    let flavor = $("#flavor-form").val();
+    let size = $("#size-form"). val();
+    let rating = $("#rating-form").val();
+    let image = $("#image-form").val();
+
+    let newCupcake = await axios.post(BASE_URL, {flavor, size, rating,image});
+
+    updateCupcakeList(newCupcake.data.cupcake.flavor)
+    
+}
+
+async function updateCupcakeList(cupcake){
+    $("#add-cupcake-form").trigger("reset");
+    let $cupcake = $(`<li>${cupcake}</li>`);
+    $cupcakeList.append($cupcake);
+
+}
+
+// populate cupcake list
+//submit cupcake form as new function name
+
+$(document).ready(populateCupcakesList)
+$("#add-cupcake-form").on("submit" , addCupcakeForm)
